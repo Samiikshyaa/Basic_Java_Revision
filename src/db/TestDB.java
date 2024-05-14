3263c708962c6cc99efe47d867e19d14d39c2b8d
Samikshya Timalsina <hce076bct034@hcoe.edu.np>
2024-05-14 21:25:29 +0545

    uncomment according to need

--- src/db/TestDB.java
+++ src/db/TestDB.java
@@ -2,8 +2,11 @@ package db;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+
+
 
 public class TestDB {
 	public static void main(String[] args) throws ClassNotFoundException, SQLException {
@@ -14,28 +17,56 @@ public class TestDB {
 		Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/testdb", "root", "12345");
 	
 		
-		//-------------------Insert SQL----------------------
+//		//-------------------Insert SQL----------------------
+//		
+//		// 1.write query
+//		String sql = "insert into user(username,password)values('samikshya','pass78') ";
+//		
+//		// 2. create statement
+//		Statement stm = con.createStatement();
+//		
+//		// 3. execute statement
+//		stm.execute(sql);
+//		
+//		//4. close db
+//		con.close();
+//		
+//		System.out.println("Data Inserted");
 		
-		// 1.write query
-		String sql = "insert into user(username,password)values('samikshya','pass78') ";
 		
-		// 2. create statement
+		
+//		//---------------------Select SQL-------------------
+//		// 1. write query
+//		String sql = "select * from user";
+//		
+//		// 2. Create statement
+//		Statement stm = con.createStatement();
+//		
+//		// 3. Execute statement
+//		ResultSet rs = stm.executeQuery(sql);
+//		
+//		while(rs.next()) {
+//			System.out.println("ID: "+ rs.getInt("id"));
+//			System.out.println("Username: "+ rs.getString("username"));
+//			System.out.println("Password: "+ rs.getString("password"));
+//			System.out.println();
+//		}
+		
+//		//------------------------Update SQL--------------
+//		String sql = "Update user set username = 'Ramesh' where username ='Ram'";
+//		Statement stm = con.createStatement();
+//		stm.execute(sql);
+//		
+//		System.out.println("Updated");
+//		
+		
+		//-------------------------DELETE SQL--------------
+		String sql = "Delete from user where id = '6'";
 		Statement stm = con.createStatement();
-		
-		// 3. execute statement
 		stm.execute(sql);
 		
-		//4. close db
-		con.close();
-		
-		System.out.println("Data Inserted");
-		
-		
-		
-		//---------------------Select SQL-------------------
-		// 1. write query
-		// 2. Create statement
-		// 3. Execute statement
+		System.out.println("Deleted");
 		
 	}
+	
 }
