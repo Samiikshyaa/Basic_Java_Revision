2394aee7257bbbfdb86d3452a58c57410878697e
Samiikshyaa <hce076bct034@hcoe.edu.np>
2024-05-29 15:07:18 +0545

    Update

--- src/basic/ArrayInterview.java
+++ src/basic/ArrayInterview.java
@@ -3,6 +3,7 @@ package basic;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
 
 public class ArrayInterview {
@@ -10,7 +11,7 @@ public class ArrayInterview {
 		int [] numbers = {1,2,3,4,5,1,3,4};
 		Set<Integer> un = new HashSet<>();
 		List<Integer> cn = new ArrayList<>();
-		
+
 		for (int i = 0; i < numbers.length; i++) {
 			un.add(numbers[i]);
 			cn.add(numbers[i]);
